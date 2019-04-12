bb9bf861565917d8ba00c6632508c1ea5e90e16f
Ryo Ota <nwtgck@gmail.com>
2019-04-12 12:37:31 +0900

    [impl] Create a basic record-send and receive-play system

--- app/src/main/java/io/github/nwtgck/piping_phone/MainActivity.kt
+++ app/src/main/java/io/github/nwtgck/piping_phone/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.github.nwtgck.piping_phone
 
+import android.annotation.SuppressLint
 import android.media.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -14,6 +15,9 @@ import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.math.max
 import android.media.AudioTrack.MODE_STREAM
+import android.widget.EditText
+import java.io.BufferedInputStream
+import java.security.SecureRandom
 
 
 // Record audio
@@ -67,7 +71,7 @@ fun recordAudio(callback: (ByteArray) -> Unit){
         }
 
         override fun onMarkerReached(recorder: AudioRecord) {
-//            recorder.read(audioDataArray, 0, oneFrameDataCount)
+            recorder.read(audioDataArray, 0, oneFrameDataCount)
             Log.i("AudioRecord", "mark size=${audioDataArray.size}")
         }
     })
@@ -78,6 +82,21 @@ fun recordAudio(callback: (ByteArray) -> Unit){
     Log.i("in recordAudio","after start")
 }
 
+fun getPath(fromId: String, toId: String): String {
+    // TODO: Use SHA256
+    return "phone/${fromId}-to-${toId}"
+}
+
+fun randomConnectId(stringLen: Int): String {
+    // (from: https://www.baeldung.com/kotlin-random-alphanumeric-string)
+    val random = SecureRandom()
+    val charPool : List<Char> = listOf('a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    val randomString = (1..stringLen)
+        .map { i -> random.nextInt(charPool.size) }
+        .map(charPool::get)
+        .joinToString("");
+    return randomString
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -85,106 +104,147 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Record button
-        val recordButton = findViewById<Button>(R.id.record_button)
+        // Server URL edit
+        val serverUrlEdit: EditText = findViewById(R.id.server_url)
+        // Connect ID edit
+        val connectIdEdit: EditText = findViewById(R.id.connect_id)
+        // Peer's connect ID edit
+        val peerConnectIdEdit: EditText = findViewById(R.id.connection_id)
+        // Connect button
+        val connectButton: Button = findViewById(R.id.connect_button)
 
+        // Set random connect ID
+        connectIdEdit.setText(randomConnectId(3))
+
+        // On connect button clicked
+        connectButton.setOnClickListener {
+            // Get server base URL
+            val serverUrl: String = serverUrlEdit.text.toString()
+            // Get connect ID
+            val connectId: String = connectIdEdit.text.toString()
+            // Get peer's connect ID
+            val peerConnectId: String = peerConnectIdEdit.text.toString()
+
+            // Record and send sound
+            recordAndSendSound(serverUrl, connectId, peerConnectId)
+
+            // Receive and play sound
+            receiveAndPlaySound(serverUrl, connectId, peerConnectId)
+        }
+    }
+
+    private fun recordAndSendSound(serverUrl: String, connectId: String, peerConnectId: String) {
         // TODO: Remove
         var cnt = 0
 
         val pOut = PipedOutputStream()
         val pIn = PipedInputStream(pOut)
-        recordButton.setOnClickListener {
 
-            // (from: http://ytch.hatenablog.com/entry/2013/07/21/213130)
-            val audioTrack = AudioTrack(
-                AudioManager.STREAM_VOICE_CALL,
-                44100, // TODO: hard code
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT,
-                (44100 / 10) * 2, // TODO: hard code
-                MODE_STREAM
-            )
-            audioTrack.play()
+        recordAudio { audioArray ->
+            cnt += 1
+            Toast.makeText(applicationContext, cnt.toString(), Toast.LENGTH_LONG).show()
+            pOut.write(audioArray)
+        }
 
-            recordAudio { audioArray ->
-                cnt += 1
-                Toast.makeText(applicationContext, cnt.toString(), Toast.LENGTH_LONG).show()
-                pOut.write(audioArray)
-            }
+        // (from: https://qiita.com/furu8ma/items/0194a69a50aa62b8aa6c)
+        val task = @SuppressLint("StaticFieldLeak")
+        object : AsyncTask<Void, Void, Void>() {
+            override fun doInBackground(vararg params: Void): Void? {
+                var con: HttpURLConnection? = null
+                try {
+                    // Create URL string
+                    // TODO: Construct URL not to use "/" manually
+                    val urlStr = "${serverUrl}/${getPath(connectId, peerConnectId)}"
+                    Log.i("urlStr", urlStr)
+                    val url = URL(urlStr)
+                    val con = url.openConnection() as HttpURLConnection
+                    con.requestMethod = "POST"
+                    con.instanceFollowRedirects = false
+                    con.doInput = true
+                    con.doOutput = true
+                    con.allowUserInteraction = true
+                    con.setChunkedStreamingMode(10)
+                    con.connect()
 
-            // (from: https://qiita.com/furu8ma/items/0194a69a50aa62b8aa6c)
-            val getTask = object : AsyncTask<Void, Void, Void>() {
-                override fun doInBackground(vararg params: Void): Void? {
-                    var con: HttpURLConnection? = null
-                    try {
-                        // TODO: hard code
-                        val urlStr = "http://ppng.ml/hoge4"
-                        val url = URL(urlStr)
-                        con = url.openConnection() as HttpURLConnection
-                        con.requestMethod = "GET"
-                        con.instanceFollowRedirects = false
-                        con.doInput = true
-                        con.doOutput = false
-//                        con.allowUserInteraction = true
-                        con.setChunkedStreamingMode(256)
-                        con.connect()
+                    val os = con.outputStream
 
-                        val iStream = con.inputStream
-
-                        val bytes = ByteArray((44100 / 10) * 2) // TODO: Hard code
-                        var read = 0
-                        while({read = iStream.read(bytes);  read}() > 0) {
-//                            Log.i("read size", read.toString())
-//                            Log.i("bytes:", bytes.toList().toString())
-                            audioTrack.write(bytes, 0, read)
-                        }
-
-                    } catch (e: InterruptedException) {
-                        Log.i("error", e.message)
-                    } finally {
-                        con?.disconnect()
+                    val bytes = ByteArray(16)
+                    while (pIn.read(bytes) > 0) {
+                        os.write(bytes)
                     }
-                    return null
+
+                } catch (e: Throwable) {
+                    Log.i("error", e.message)
+                } finally {
+                    con?.disconnect()
                 }
-            }
-            getTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
-
-
-            if(false) {
-                // (from: https://qiita.com/furu8ma/items/0194a69a50aa62b8aa6c)
-                val task = object : AsyncTask<Void, Void, Void>() {
-                    override fun doInBackground(vararg params: Void): Void? {
-                        var con: HttpURLConnection? = null
-                        try {
-                            // TODO: hard code
-                            val urlStr = "http://ppng.ml/hoge2"
-                            val url = URL(urlStr)
-                            con = url.openConnection() as HttpURLConnection
-                            con.requestMethod = "POST"
-                            con.instanceFollowRedirects = false
-                            con.doInput = true
-                            con.doOutput = true
-                            con.allowUserInteraction = true
-                            con.setChunkedStreamingMode(10)
-                            con.connect()
-
-                            val os = con.outputStream
-
-                            val bytes = ByteArray(16)
-                            while (pIn.read(bytes) > 0) {
-                                os.write(bytes)
-                            }
-
-                        } catch (e: InterruptedException) {
-                            Log.i("error", e.message)
-                        } finally {
-                            con?.disconnect()
-                        }
-                        return null
-                    }
-                }
-                task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+                return null
             }
         }
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+    }
+
+    fun receiveAndPlaySound(serverUrl: String, connectId: String, peerConnectId: String) {
+        // Buffer size for audio track
+        val bufSize = AudioTrack.getMinBufferSize(
+            44100,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        // (from: http://ytch.hatenablog.com/entry/2013/07/21/213130)
+        val audioTrack = AudioTrack(
+            AudioManager.STREAM_VOICE_CALL,
+            44100, // TODO: hard code
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            bufSize,
+            MODE_STREAM
+        )
+        Toast.makeText(applicationContext, "bufSize: "+bufSize.toString(), Toast.LENGTH_LONG).show()
+        audioTrack.play()
+
+        // (from: https://qiita.com/furu8ma/items/0194a69a50aa62b8aa6c)
+        val getTask = @SuppressLint("StaticFieldLeak")
+        object : AsyncTask<Void, Void, Void>() {
+            override fun doInBackground(vararg params: Void): Void? {
+                var con: HttpURLConnection? = null
+                try {
+                    // Create URL string
+                    // TODO: Construct URL not to use "/" manually
+                    val urlStr = "${serverUrl}/${getPath(peerConnectId, connectId)}"
+                    Log.i("urlStr", urlStr)
+                    val url = URL(urlStr)
+                    con = url.openConnection() as HttpURLConnection
+                    con.requestMethod = "GET"
+                    con.instanceFollowRedirects = false
+                    con.doInput = true
+                    con.doOutput = false
+                    con.setChunkedStreamingMode(256)
+                    con.connect()
+
+                    val iStream = BufferedInputStream(con.inputStream, bufSize)
+                    val bytes = ByteArray(bufSize)
+                    while(true) {
+                        // Fill bytes until getting bytes.size except the last otherwise have noses
+                        var read = 0
+                        var totalRead = 0
+                        do {
+                            read = iStream.read(bytes, totalRead, bytes.size - totalRead)
+                            if(read < 0) break
+                            totalRead += read
+                        } while (totalRead != bytes.size)
+                        audioTrack.write(bytes, 0, totalRead)
+                        if(read < 0) break
+                    }
+
+                } catch (e: Throwable) {
+                    Log.i("error", e.message)
+                } finally {
+                    con?.disconnect()
+                }
+                return null
+            }
+        }
+        getTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
     }
 }
